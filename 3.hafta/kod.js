bfd36a18e9ac8335937e1e1ebc406b9ef86ad495
Neslihan Akson <119112526+neslihanaksnn@users.noreply.github.com>
2023-08-01 14:38:12 +0300

    Add files via upload

---
+++ 3.hafta/kod.js
@@ -0,0 +1,29 @@
+var ogrenciler=["ali","nesli","irem","can"]
+        console.log(ogrenciler);
+        // ogrenciler.push("alicabbar") //sona  ekler
+        // console.log(ogrenciler);
+        // console.log(ogrenciler[i]);
+
+        ogrenciler.shift()//bastan ilk elemani siler kalıcı olarak
+        console.log(ogrenciler);
+
+        ogrenciler.pop()
+        console.log(ogrenciler);//sondan ilk elemani siler kalıcı olarak
+
+        var yeniler=["aslı","sıla","eda"]
+        console.log(ogrenciler.concat(yeniler));//sona ekler ama gecici olarak
+
+        console.log(ogrenciler);
+
+        console.log(ogrenciler.sort());//alfabetik olarak sıralar
+        console.log(ogrenciler.length);//boyutu verir
+
+        
+
+
+
+
+
+
+
+
---
+++ 3.hafta/sozlukler.js
@@ -0,0 +1,11 @@
+var ogrenci={
+    ad:"neslihan",
+    soyad:"akson",
+    yas:19,
+    email:"neslihanakson@gmail.com",
+    aldigiderssler:["react","java","node.js"],
+    selamVer:function(){return console.log("selam");}
+}
+console.log(ogrenci.selamVer());
+console.log(ogrenci.ad);
+console.log(ogrenci.aldigiderssler[1]);
